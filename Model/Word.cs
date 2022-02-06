ed3caa23167308a0295137b3e5061325680f685c
Emiliia Nazarenko <naz.emiliya@mail.ru>
2022-02-06 21:01:55 +0100

    upload solution

---
+++ Model/Word.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReaderHelper.Model
+{
+	public class Word : INotifyPropertyChanged
+	{
+		private string _initialWord;
+		public string InitialWord
+		{
+			get
+			{
+				return _initialWord;
+			}
+			set
+			{
+				_initialWord = value;
+				OnPropertyChanged(InitialWord);
+			}
+		}
+
+		private string _translatedWord;
+		public string TranslatedWord
+		{
+			get
+			{
+				return _translatedWord;
+			}
+			set
+			{
+				_translatedWord = value;
+				OnPropertyChanged(TranslatedWord);
+			}
+		}
+
+		#region INotifyPropertyChanged Members 
+		public event PropertyChangedEventHandler PropertyChanged;
+		private void OnPropertyChanged(string propertyName)
+		{
+			if (PropertyChanged != null)
+			{
+				PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+		#endregion
+	}
+}
+
+
+
